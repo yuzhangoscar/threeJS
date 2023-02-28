51084d8316142aeb0fac14758f3a694d2077d5a7
Bruce Chang <brucechang@192-168-1-108.tpgi.com.au>
2023-02-28 21:27:13 +1100

    [CODE] added a lightblue cube in the middle

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log(THREE);
+const canvas = document.querySelector(".webgl");
+const scene = new THREE.Scene();
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1); 
+const boxMaterial = new THREE.MeshBasicMaterial({color: "lightblue"});
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(boxMesh);
+
+const cameraSize = {
+    width: 800,
+    height: 600
+};
+const camera = new THREE.PerspectiveCamera(75, cameraSize.width/cameraSize.height);
+camera.position.z = 3;
+camera.position.x = 1;
+camera.position.y = 1;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(cameraSize.width, cameraSize.height);
+renderer.render(scene, camera);
