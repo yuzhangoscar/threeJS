2e9cd410ffd9851621d8928130bcee6ff4323f0b
Bruce Chang <brucechang@192-168-1-108.tpgi.com.au>
2023-03-04 21:34:21 +1100

    [CODE] added full screen and resize support

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
@@ -7,20 +9,22 @@ const boxMaterial = new THREE.MeshBasicMaterial({color: "lightblue", wireframe: 
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 const axeHelper = new THREE.AxesHelper();
 const clock = new THREE.Clock();
-let cameraPosition = new THREE.Vector3();
 scene.add(boxMesh);
 scene.add(axeHelper);
 
 const cameraSize = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 };
 const cursor = {
     x: 0,
     y: 0
 };
-const camera = new THREE.PerspectiveCamera(75, cameraSize.width/cameraSize.height, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(90, cameraSize.width/cameraSize.height, 0.1, 1000);
+camera.position.z = 3;
+camera.lookAt(boxMesh.position);
 scene.add(camera);
+const controls = new OrbitControls(camera, canvas);
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -30,16 +34,30 @@ renderer.setSize(cameraSize.width, cameraSize.height);
 function tick() {
     const elapsedTime = clock.getElapsedTime();
 
-    camera.position.z = Math.cos(cursor.x * Math.PI * 3);
-    camera.position.x = Math.sin(cursor.x * Math.PI * 3);
-    //camera.position.y = -1*(event.clientY/cameraSize.height-0.5)*2;
-    camera.lookAt(boxMesh.position);
-
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
 }
 
 tick();
+
+window.addEventListener('dblclick', (event) => {
+    console.log('double clicked');
+    if(!document.fullscreenElement) {
+        canvas.requestFullscreen();
+    }
+    else {
+        document.exitFullscreen();
+    }
+});
+
+window.addEventListener('resize', (event) => {
+    console.log('resize');
+    cameraSize.width = window.innerWidth;
+    cameraSize.height = window.innerHeight;
+    camera.aspect = cameraSize.width / cameraSize.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(cameraSize.width, cameraSize.height);
+});
 
 window.addEventListener("mousemove", (event) => {
     cursor.x = event.clientX / cameraSize.width - 0.5;
